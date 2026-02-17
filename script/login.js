914f08d89adcabafc270b53ed71ece0b75450202
amirulislambd <amirulislambd313@gmail.com>
2026-02-18 00:15:53 +0600

    added navber and welcome to payoo sectons

---
+++ script/login.js
@@ -0,0 +1,17 @@
+document.getElementById('login-bg').addEventListener('click',function(){
+    const numberInput = document.getElementById('phone-number')
+    const contactNumber = numberInput.value
+    const passwordInput = document.getElementById('password')
+
+    
+
+    const password = passwordInput.value
+    // console.log(contactNumber, password)
+    if(contactNumber == '01814022917' && password.length == 4){
+        alert('Login Success')
+
+        window.location.replace('/home.html')
+    }else{
+        alert('Login failed')
+    }
+})
