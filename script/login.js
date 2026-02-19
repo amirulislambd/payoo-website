85776b2c36c041026fd38480f05ab2717c2e80f3
amirulislambd <amirulislambd313@gmail.com>
2026-02-19 17:47:55 +0600

    update login section

--- script/login.js
+++ script/login.js
@@ -1,17 +1,23 @@
-document.getElementById('login-btn').addEventListener('click',function(){
-    const numberInput = document.getElementById('phone-number')
-    const contactNumber = numberInput.value
-    const passwordInput = document.getElementById('password')
-
-    
-
+document.getElementById('login-btn').addEventListener('click',function(e){
+    e.preventDefault()
+    const inputNumber =document.getElementById('phone-number')
+    const contactNumber = inputNumber.value
+    const numberPattern = /^01\d{9}$/
+    const passwordInput =document.getElementById('password')
     const password = passwordInput.value
     // console.log(contactNumber, password)
-    if(contactNumber == '01814022917' && password.length == 4){
-        alert('Login Success')
+    if(!numberPattern.test(contactNumber)){
+        alert('Please Enter your valid phone number and start with 01')
+        return
 
-        window.location.replace('./home.html')
-    }else{
+        
+    }else if(contactNumber.length <11 && password.length == 4){
         alert('Login failed')
+        return
+    }else if(password < 4){
+        alert('Wrong Your Password')
+        return
+    }else{
+        window.location.replace('./home.html')
     }
 })
